c6dfc16161a18c1447da94c781cca46690df39c5
Mayur Pai B H <mayur5pai@gmail.com>
2020-07-03 01:17:06 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(window).resize(function() {
-    var mobileWidth = (window.innerWidth > 0) ?
-        window.innerWidth :
-        screen.width;
-    var viewport = (mobileWidth > 360) ?
-        'width=device-width, initial-scale=1.0' :
-        'width=1200';
-    $("meta[name=viewport]").attr('content', viewport);
-}).resize();
