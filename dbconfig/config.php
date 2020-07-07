a9b67a8b8a18a4a2abedbcc37b826b0fb84749f2
Mayur Pai B H <mayur5pai@gmail.com>
2020-07-07 15:08:25 +0530

    Delete config.php

--- dbconfig/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-/*For My LocalPC*/
-$con=mysqli_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
-mysqli_select_db ($con,'contact');
-?>
